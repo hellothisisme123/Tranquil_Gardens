82a2b2fae262e3eed661b9f86ff4b2ca0667b300
Luke Lawton <115205055+hellothisisme123@users.noreply.github.com>
2023-02-02 13:42:58 -0500

    is it platinum is it gold, shop, finished faq

--- js/faq.js
+++ js/faq.js
@@ -1,10 +1,11 @@
 let questions = document.querySelectorAll('.question')
 
 questions.forEach(question => {
-    question.addEventListener('click', (e) => {
-        if (e.target.classList.length > 0) {
-            console.log('direct');
-        } else if (e.target.classList.length > 0)
-        console.log(e.target);
+    question.addEventListener('click', (e, target) => {
+        if (e.target.dataset.state) target = e.target
+        else if (e.target.parentElement.dataset.state) target = e.target.parentElement
+
+        if (target.dataset.state == 'up') target.dataset.state = 'down'
+        else if (target.dataset.state == 'down') target.dataset.state = 'up'
     })
 })
