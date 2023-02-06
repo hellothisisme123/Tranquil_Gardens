790d472986cf2e2493daee224082de943855534c
Luke Lawton <115205055+hellothisisme123@users.noreply.github.com>
2023-02-06 14:26:24 -0500

    school stuff idk

--- js/purchaseSingleItem.js
+++ js/purchaseSingleItem.js
@@ -1,4 +1,39 @@
 let itemData = Object.fromEntries(new URLSearchParams(location.search))
 console.log(itemData);
 
-window.onload()
+const title = document.querySelector('.title'),
+      cost = document.querySelector('.cost'),
+      starRatingWrapper = document.querySelector('.starRatingWrapper'),
+      ratingCount = document.querySelector('.ratingCount'),
+      description = document.querySelector('.description'),
+      img = document.querySelector('.imgWrapper > img')
+      stock = document.querySelector('.stock')
+
+const formatter = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD'
+})
+
+title.innerHTML = itemData.name
+description.innerHTML = itemData.description
+cost.innerHTML = formatter.format(itemData.price)
+ratingCount.innerHTML = `${Math.floor(Math.random()*258)} Ratings`
+stock.innerHTML = `Only ${itemData.stock} Left`
+
+img.src = `./production/images/shop/${itemData.imgName}`
+img.alt = itemData.name
+
+// fills in the stars accordingly to the starCount
+const starCount = Math.floor(Math.random() * 5 + 1)
+Array.from(starRatingWrapper.children).forEach((star, i) => {
+    if (star.tagName != 'I') return
+    
+    if (starCount - i > 0) {
+        star.classList.remove('fa-regular')
+        star.classList.add('fa-solid')
+    } else {
+        star.classList.add('fa-regular')
+        star.classList.remove('fa-solid')
+    }
+});
+
--- js/shop.js
+++ js/shop.js
@@ -9,6 +9,8 @@ expandBtns.forEach(btn => {
     })
 })
 
+let shoppingCart = []
+
 items.forEach(item => {
     item = {
         'select': item,
@@ -17,13 +19,71 @@ items.forEach(item => {
         'type': item.dataset.type,
         'stock': item.dataset.stock,
         'imgName': item.dataset.imgName,
+        'description': item.querySelector('.description').innerText,
         'orderNowBtn':item.querySelector('.btnWrapper').querySelector('.orderNow'),
         'addToCartBtn': item.querySelector('.btnWrapper').querySelector('.addToCart')
     }
 
-    item.orderNowBtn.href = `./purchaseSingleItem.html?name=${item.name}&price=${item.price}&type=${item.type}&stock=${item.stock}&imgName=${item.imgName}`
+    item.orderNowBtn.href = `./purchaseSingleItem.html?name=${item.name}&price=${item.price}&type=${item.type}&stock=${item.stock}&imgName=${item.imgName}&description=${item.description}`
+
+    item.addToCartBtn.addEventListener('click', e => {
+        const item = {
+            'select': e.target.parentElement.parentElement,
+            'price': e.target.parentElement.parentElement.dataset.price,
+            'name': e.target.parentElement.parentElement.dataset.name,
+            'type': e.target.parentElement.parentElement.dataset.type,
+            'stock': e.target.parentElement.parentElement.dataset.stock,
+            'imgName': e.target.parentElement.parentElement.dataset.imgName,
+            'addToCartBtn': e.target.parentElement.parentElement.querySelector('.btnWrapper').querySelector('.addToCart')
+        }
+        item.cartBtnBoundBox = item.addToCartBtn.getBoundingClientRect()
 
 
-    
-    
+
+        // creates a leaf element
+        let leaf = document.createElement('img')
+        leaf.src = './production/svg/leaf.svg'
+        leaf.classList.add('leafIcon')
+        item.addToCartBtn.appendChild(leaf)
+
+        // animated the leaf to the cart icon
+        // it takes a reasonable amount of time each time
+        let leafAniDuration = 450 * item.cartBtnBoundBox.top / 800
+        setTimeout(() => leaf.remove(), leafAniDuration / 1.025);
+        leaf.animate(
+            [
+                {
+                    left: '0',
+                    top: '0'
+                },
+                {
+                    left: `${document.documentElement.clientWidth / 1.1 - item.cartBtnBoundBox.left}px`,
+                    top: `-${item.cartBtnBoundBox.top}px`
+                }
+            ],
+            {
+                duration: leafAniDuration,
+                iterations: 1,
+                easing: 'cubic-bezier(.57,.34,.83,.67)'
+                
+            }
+        )
+
+        
+        // adds item to cart cookie
+        shoppingCart.push(item)
+        console.log(shoppingCart);
+
+        const date = new Date()
+        date.setTime(date.getTime() + 2592000000)
+        document.cookie = `shoppingCart=${JSON.stringify(shoppingCart)};expired=${date.toUTCString()};path=/;SameSite=None;Secure`
+        
+        
+
+        console.log(tmpCookies);
+        
+
+
+
+    })
 })
